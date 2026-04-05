b46111615c3eb79ae0bca25bc295342639f0dbc3
Sachal Mohar <sachal16@uvic.ca>
2026-04-04 22:22:43 -0700

    views.py GET requests/ searches for catergory/tags/products

--- products/models.py
+++ products/models.py
@@ -24,16 +24,19 @@ class Tag(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=200)
 
-    description = models.TextField()
     # we need search so product decription
+    description = models.TextField()
+
 
     price = models.DecimalField(max_digits=10, decimal_places= 2)
 
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     # use Foriegn Key to make column id of whichever category , ondelete deletes all products in the category
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
-    tags = models.ManyToManyField(Tag, blank=True)
+
     #product can have many tags, and tag can have many products/ True cause not all products have tags
+    tags = models.ManyToManyField(Tag, blank=True)
+
 
     def __str__(self):
         return self.name
--- products/views.py
+++ products/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product, Category, Tag
+
+
+def product_list(request):
+
+    #grab all products from the database
+    products = Product.objects.all()
+
+    #GET request to  grab search value or empty (repeat for catergory/tags)
+    search_query = request.GET.get('search', '')
+    category_id =  request.GET.get('category', '')
+    tag_ids = request.GET.getlist('tags') # made getlist to grab all tags with multiple vals
+
+    if search_query:
+        #run if isn't empty, narrow the list ot find matching items case insenstitive
+        products = products.filter(description__icontains=search_query)
+    if category_id:
+        #on products where id matches
+        products = products.filter(category_id=category_id)
+    if tag_ids:
+        #remove dupes finds tag in particular list of ids
+        products = products.filter(tags__in=tag_ids).distinct()
+
+    #filter dropdowns
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
+
+    #filter dropdowns
+    context = {
+        'products': products,
+        'categories': categories,
+        'tags': tags,
+        'search_query': search_query,
+        'selected_category': category_id,
+        'selected_tags': tag_ids,
+    }
+
+    # fill the template with context data, have path to our template
+    return render(request, 'products/product_list.html', context)
+
+
