a31de8aba71a1ad6aed2218c29e78459635b8d98
Sachal Mohar <sachal16@uvic.ca>
2026-04-04 20:26:53 -0700

    admin setup done, can see Categorys, Products, and Tags in PRODUCTS section

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Tag, Product
 
-# Register your models here.
+#adds each to admin panel
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Product)
