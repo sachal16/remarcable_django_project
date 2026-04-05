188deb3937d29e3f41a2d0d3b3a18865b75956c5
Sachal Mohar <sachal16@uvic.ca>
2026-04-04 19:44:33 -0700

    made models, migrated; tables built

--- products/models.py
+++ products/models.py
@@ -13,9 +13,27 @@ class Category(models.Model):
 
 class Tag(models.Model):
     #models.Model - initilaizing a tag database table
+
     name = models.CharField(max_length=99)
 
     #displays name of tag object
     def __str__(self):
         return self.name
 
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+
+    description = models.TextField()
+    # we need search so product decription
+
+    price = models.DecimalField(max_digits=10, decimal_places= 2)
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # use Foriegn Key to make column id of whichever category , ondelete deletes all products in the category
+
+    tags = models.ManyToManyField(Tag, blank=True)
+    #product can have many tags, and tag can have many products/ True cause not all products have tags
+
+    def __str__(self):
+        return self.name
