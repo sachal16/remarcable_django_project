0f1e7fba3dd719746cfc766d0f1a7c9cda9073f5
Sachal Mohar <sachal16@uvic.ca>
2026-04-04 19:12:52 -0700

    catergoy database & tag database setup

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    # models.Model - initilaizing a category database table
+
+    #column in catergory table
+    name = models.CharField(max_length=99)
+
+    #displays name of catogory object
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    #models.Model - initilaizing a tag database table
+    name = models.CharField(max_length=99)
+
+    #displays name of tag object
+    def __str__(self):
+        return self.name
+
